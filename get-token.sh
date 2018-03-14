25ec34ef60d086261d9683987f33404581918370
Chris Duchesne <cduchesne@gmail.com>
2018-03-13 18:14:30 -0700

    consolidating to use labels only labels now utilized in all necessary sections to support remote attacher properly

--- get-token.sh
+++ get-token.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-secret=$(oc get sa scaleio-flex-volume --template='{{range .secrets}}{{.name}}{{"\n"}}{{end}}' | grep token)
-token=$(oc get secret $secret --template='{{ .data.token}}' | base64 -d)
+secret=$(oc --config=/path/to/kubeconfig get sa scaleio-flex-volume --template='{{range .secrets}}{{.name}}{{"\n"}}{{end}}' | grep token)
+token=$(oc --config=/path/to/kubeconfig get secret $secret --template='{{ .data.token}}' | base64 -d)
 
 echo -n $token
