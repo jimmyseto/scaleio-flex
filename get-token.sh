7f3a9ce9937302321fcd68ad2969fc99ae4e9d15
jseto <jimmy.seto@emc.com>
2018-03-30 14:17:45 -0400

    - adding autodetection of SELinux status - setting SELINUX_ENABLED accordingly - changing default KUBECONFIG to one that can access secret/token as needed

--- get-token.sh
+++ get-token.sh
@@ -1,6 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 
-secret=$(oc --config=/path/to/kubeconfig get sa scaleio-flex-volume --template='{{range .secrets}}{{.name}}{{"\n"}}{{end}}' | grep token)
-token=$(oc --config=/path/to/kubeconfig get secret $secret --template='{{ .data.token}}' | base64 -d)
+# Set KUBECONFIG to point to desired config file; ensure specified config file exists on all nodes 
+KUBECONFIG=/root/.kube/config
 
-echo -n $token
+# Set SERVICE_ACC to desired/existing service account; KUBECONFIG needs to have permissions to get secret/ticket from SERVICE_ACC 
+SERVICE_ACC=scaleio-flex-volume
+
+secret=$(oc --config=${KUBECONFIG} get sa ${SERVICE_ACC} --template='{{range .secrets}}{{.name}}{{"\n"}}{{end}}' | grep token)
+token=$(oc --config=${KUBECONFIG} get secret ${secret} --template='{{ .data.token}}' | base64 -d)
+
+echo -n ${token}
