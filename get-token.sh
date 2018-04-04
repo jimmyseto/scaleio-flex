f0375c540e87f57bd55c9344441e4c0a90e94359
jseto <jimmy.seto@emc.com>
2018-04-04 16:29:05 -0400

    making compatible with kubernetes/kubectl

--- get-token.sh
+++ get-token.sh
@@ -6,7 +6,17 @@ KUBECONFIG=/root/.kube/config
 # Set SERVICE_ACC to desired/existing service account; KUBECONFIG needs to have permissions to get secret/ticket from SERVICE_ACC 
 SERVICE_ACC=scaleio-flex-volume
 
-secret=$(oc --config=${KUBECONFIG} get sa ${SERVICE_ACC} --template='{{range .secrets}}{{.name}}{{"\n"}}{{end}}' | grep token)
-token=$(oc --config=${KUBECONFIG} get secret ${secret} --template='{{ .data.token}}' | base64 -d)
+WHICH_OC=$(which oc > /dev/null 2>&1)
+WHICH_OC_RETURN=$(echo $?)
+if [ "${WHICH_OC_RETURN}" = "0" ]; then
+	CMD="oc"
+	CONFIG_OPTION="--config"
+else
+	CMD="kubectl"
+	CONFIG_OPTION="--kubeconfig"
+fi
+
+secret=$(${CMD} ${CONFIG_OPTION}=${KUBECONFIG} get sa ${SERVICE_ACC} --template='{{range .secrets}}{{.name}}{{"\n"}}{{end}}' | grep token)
+token=$(${CMD} ${CONFIG_OPTION}=${KUBECONFIG} get secret ${secret} --template='{{ .data.token}}' | base64 -d)
 
 echo -n ${token}
