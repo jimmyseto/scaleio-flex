36161328e0fa858ccda25881785d132465b21f8b
jseto <jimmy.seto@emc.com>
2018-04-09 11:04:26 -0400

    port latest fixes/enhancements made to scaleio to scaleio-simple (logging, automatic SELinux detection, etc); port support for config file separation to get-token.sh

--- get-token.sh
+++ get-token.sh
@@ -1,10 +1,21 @@
 #!/bin/sh
-
-# Set KUBECONFIG to point to desired config file; ensure specified config file exists on all nodes 
-KUBECONFIG=/root/.kube/config
+#
+# ScaleIO FlexVolume utility for obtaining token 
+#
+# Do not modify thist script, configuration settings can be found at
+# /opt/emc/scaleio/flexvolume/cfg/config
 
 # Set SERVICE_ACC to desired/existing service account; KUBECONFIG needs to have permissions to get secret/ticket from SERVICE_ACC 
 SERVICE_ACC=scaleio-flex-volume
+
+CONFIGFILE="/opt/emc/scaleio/flexvolume/cfg/config"
+if [ -x "${CONFIGFILE}" ]; then
+  # source the config file
+        source "${CONFIGFILE}"
+fi
+
+# in case some values were not specific in the config file, set their values
+KUBECONFIG="${KUBECONFIG:-/root/.kube/config}"
 
 WHICH_OC=$(which oc > /dev/null 2>&1)
 WHICH_OC_RETURN=$(echo $?)
